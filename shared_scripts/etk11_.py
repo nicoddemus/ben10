9ecf34105261e5fe1bdec21f442a1a1ca9166f2c
Alexandre Andrade <ama@esss.com.br>
2013-08-11 20:31:22 -0300

    Added shared-script.

---
+++ shared_scripts/etk11_.py
@@ -0,0 +1,19 @@
+from sharedscripts10.shared_scripts.esss_project import EsssProject
+
+
+
+#===================================================================================================
+# Etk11
+#===================================================================================================
+class Etk11(EsssProject):
+
+    NAME = 'etk11'
+    DEPENDENCIES = [
+        'ftputil',
+        'ordereddict',
+        'path_py',
+        'pywin32',
+    ]
+
+    NAMESPACE_VARIABLES = {
+    }
