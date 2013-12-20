d625d58e0f30c49bb4a03b94fa041a8390df2ab7
Alexandre Andrade <ama@esss.com.br>
2013-12-20 16:24:12 -0200

    More coverage

--- source/python/ben10/filesystem/_duplicates.py
+++ source/python/ben10/filesystem/_duplicates.py
@@ -71,36 +71,6 @@ class ExtendedPathMask(object):
 
 
 #===================================================================================================
-# GetMTime
-#===================================================================================================
-def GetMTime(path):
-    '''
-    :param str path:
-        Path to file or directory
-
-    :rtype: float
-    :returns:
-        Modification time for path.
-
-        If this is a directory, the highest mtime from files inside it will be returned.
-
-    @note:
-        In some Linux distros (such as CentOs, or anything with ext3), mtime will not return a value
-        with resolutions higher than a second.
-
-        http://stackoverflow.com/questions/2428556/os-path-getmtime-doesnt-return-fraction-of-a-second
-    '''
-    if os.path.isdir(path):
-        files = FindFiles(path)
-
-        if len(files) > 0:
-            return max(map(os.path.getmtime, files))
-
-    return os.path.getmtime(path)
-
-
-
-#===================================================================================================
 # CheckForUpdate
 #===================================================================================================
 def CheckForUpdate(source, target):
