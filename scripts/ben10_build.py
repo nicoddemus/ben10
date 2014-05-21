b84fc779710ea412b34ff5a64192edb16391ca81
Diogo de Campos <campos@esss.com.br>
2014-05-21 09:42:43 -0300

    BEN-16 Use project.run_tests in ben10 build (for cache)

--- scripts/ben10_build.py
+++ scripts/ben10_build.py
@@ -1,4 +1,4 @@
-from aasimar10.shared_commands import BuildCommand, BuildStopped
+from aasimar10.shared_commands import BuildCommand
 from ben10.foundation.decorators import Override
 
 
@@ -8,18 +8,6 @@ from ben10.foundation.decorators import Override
 #===================================================================================================
 class Ben10BuildCommand(BuildCommand):
 
-    name = 'Ben10BuildCommand'
-
     @Override(BuildCommand.EvBuild)
     def EvBuild(self, args):
-        from sharedscripts10.shared_scripts.python_ import Python
-        _output, retcode = Python().Execute2(
-            self.shared_script.Evaluate('`self.working_dir`/ci.py'),
-            output_callback=self.oss.P,
-            cwd=self.shared_script['working_dir']
-        )
-        if retcode == 999:  # Raised by ci.py when tests fail
-            raise BuildStopped('Tests failed')
-        elif retcode:
-            raise RuntimeError('ci.py failed with retcode ' + str(retcode))
-
+        self.RunTests(jobs=6, xml=True)
